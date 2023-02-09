8bfd4af873d740b946bb7e3ea59350e55b7f26b2
“SHENEL98” <wsakurera@gmail.com>
2023-02-09 18:21:12 +0100

    final commit of simple one page react application

---
+++ components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Why choose React</h1>
+            <ul className="main--facts">
+                <li>It's easy to learn</li>
+                <li>It helps to build rich user interfaces</li>
+                <li>It uplifts developers’ productivity</li>
+                <li>It offers fast rendering</li>
+                <li>It offer better code stability</li>
+            </ul>
+        </main>
+    )
+}
